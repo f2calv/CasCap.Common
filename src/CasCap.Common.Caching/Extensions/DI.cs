9652fad1cf12ddc2021def282a185ea8883c38bf
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2023-03-04 18:10:27 +0100

    Updated AsyncKeyedLock options to use own configuration

--- src/CasCap.Common.Caching/Extensions/DI.cs
+++ src/CasCap.Common.Caching/Extensions/DI.cs
@@ -10,19 +10,25 @@ public static class DI
         services.AddSingleton<IRedisCacheService, RedisCacheService>();
         services.AddSingleton<IDistCacheService, DistCacheService>();
         services.AddHostedService<LocalCacheInvalidationService>();
-        CachingOptions cachingOptions = new();
         services.AddSingleton<IConfigureOptions<CachingOptions>>(s =>
         {
             var configuration = s.GetService<IConfiguration?>();
-            return new ConfigureOptions<CachingOptions>(options => {
-                configuration?.Bind(CachingOptions.SectionKey, options);
-                cachingOptions = options;
+            return new ConfigureOptions<CachingOptions>(options => configuration?.Bind(CachingOptions.SectionKey, options));
+        });
+        AsyncKeyedLockPoolingOptions asyncKeyedLockPoolingOptions = new();
+        services.AddSingleton<IConfigureOptions<AsyncKeyedLockPoolingOptions>>(s =>
+        {
+            var configuration = s.GetService<IConfiguration?>();
+            return new ConfigureOptions<AsyncKeyedLockPoolingOptions>(options =>
+            {
+                configuration?.Bind(AsyncKeyedLockPoolingOptions.SectionKey, options);
+                asyncKeyedLockPoolingOptions = options;
             });
         });
         services.AddSingleton(new AsyncKeyedLocker<string>(o =>
         {
-            o.PoolSize = cachingOptions.KeyedLockPoolSize;
-            o.PoolInitialFill = cachingOptions.KeyedLockPoolInitialFill;
+            o.PoolSize = asyncKeyedLockPoolingOptions.PoolSize;
+            o.PoolInitialFill = asyncKeyedLockPoolingOptions.PoolInitialFill;
         }));
     }
 }
