c29f28dcbafb8e2496d7b742a00e4fb01882c1a1
Alex Vincent <alex@far2cool.com>
2024-07-30 09:59:07 +0200

    add AddCasCapCaching overload

--- src/CasCap.Common.Caching/Extensions/DI.cs
+++ src/CasCap.Common.Caching/Extensions/DI.cs
@@ -1,20 +1,30 @@
 ﻿using AsyncKeyedLock;
-
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DI
 {
-    public static void AddCasCapCaching(this IServiceCollection services)
+    public static void AddCasCapCaching(this IServiceCollection services, CachingOptions? cachingOptions = null)
     {
         //services.AddMemoryCache();//now added inside DistCacheService
         services.AddSingleton<IRedisCacheService, RedisCacheService>();
         services.AddSingleton<IDistCacheService, DistCacheService>();
         services.AddSingleton<AsyncKeyedLocker<string>>();
         services.AddHostedService<LocalCacheInvalidationBgService>();
-        services.AddSingleton<IConfigureOptions<CachingOptions>>(s =>
+        if (cachingOptions is null)
+            services.AddSingleton<IConfigureOptions<CachingOptions>>(s =>
+            {
+                var configuration = s.GetService<IConfiguration?>();
+                return new ConfigureOptions<CachingOptions>(options => configuration?.Bind(CachingOptions.SectionKey, options));
+            });
+        else
         {
-            var configuration = s.GetService<IConfiguration?>();
-            return new ConfigureOptions<CachingOptions>(options => configuration?.Bind(CachingOptions.SectionKey, options));
-        });
+            var options = Options.Options.Create(cachingOptions);
+            services.AddSingleton(options);
+        }
+    }
+
+    public static void AddCasCapCaching(this IServiceCollection services, string connectionString)
+    {
+        services.AddCasCapCaching(new CachingOptions { redisConnectionString = connectionString });
     }
 }
