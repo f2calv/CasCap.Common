ef9d1802161789de414a44736bf913dba80b0153
Alex Vincent <alex@far2cool.com>
2024-08-09 07:34:29 +0200

    added CasCap.Common.Serialisation.Json.Tests

---
+++ src/CasCap.Common.Serialisation.Json.Tests/Tests/TestsBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CasCap.Common.Extensions.Tests;
+
+public abstract class TestBase
+{
+    public TestBase(ITestOutputHelper output)
+    {
+        //initiate ServiceCollection w/logging
+        var services = new ServiceCollection()
+            .AddXUnitLogging(output);
+
+        //assign services to be tested
+        var serviceProvider = services.BuildServiceProvider();
+
+        ApplicationLogging.LoggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+    }
+}
---
+++ src/CasCap.Common.Serialisation.Json.Tests/Usings.cs
@@ -0,0 +1,8 @@
+﻿global using CasCap.Common.Extensions;
+global using CasCap.Common.Extensions.Tests;
+global using Microsoft.Extensions.DependencyInjection;
+global using Newtonsoft.Json;
+global using System;
+global using System.Collections.Generic;
+global using Xunit;
+global using Xunit.Abstractions;
