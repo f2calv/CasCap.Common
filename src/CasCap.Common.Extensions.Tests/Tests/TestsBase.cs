4b5784daba825a202a8bf925554edfd9fa194dc3
Alex Vincent <alex@far2cool.com>
2024-08-09 07:33:46 +0200

    tidy

--- src/CasCap.Common.Extensions.Tests/Tests/TestsBase.cs
+++ src/CasCap.Common.Extensions.Tests/Tests/TestsBase.cs
@@ -2,24 +2,13 @@
 
 public abstract class TestBase
 {
-    //protected I???Service _???Svc;
-
     public TestBase(ITestOutputHelper output)
     {
-        var configuration = new ConfigurationBuilder()
-            //.AddCasCapConfiguration()
-            .Build();
-
         //initiate ServiceCollection w/logging
         var services = new ServiceCollection()
-            .AddSingleton<IConfiguration>(configuration)
             .AddXUnitLogging(output);
-
-        //add services
-        //services.Add???();
 
         //assign services to be tested
         _ = services.BuildServiceProvider();
-        //_???Svc = serviceProvider.GetRequiredService<I???Service>();
     }
 }
