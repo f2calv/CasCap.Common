5c98cedb4299648b5d5aa89bebbfff72befa993a
Alex Vincent <alex@far2cool.com>
2024-08-09 06:23:02 +0200

    refactor converters

--- src/CasCap.Common.Serialisation.Json/Converters/JsonConverters.cs
+++ src/CasCap.Common.Serialisation.Json/Converters/JsonConverters.cs
@@ -1,72 +1,5 @@
 ﻿namespace CasCap.Models;
 
-//https://stackoverflow.com/questions/10302902/can-you-tell-json-net-to-serialize-datetime-as-utc-even-if-unspecified/10305908
-/*
-public class UtcJsonDateTimeConverter : JsonConverter
-{
-    const string DefaultDateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFZ";
-
-    public override bool CanConvert(Type objectType)
-    {
-        return objectType == typeof(DateTime);
-    }
-
-    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    {
-        bool nullable = objectType == typeof(DateTime?);
-        if (reader.TokenType == JsonToken.Null)
-        {
-            if (!nullable) throw new JsonSerializationException($"Cannot convert null value to {objectType}.");
-            return null;
-        }
-
-        if (reader.TokenType == JsonToken.Date)
-            return reader.Value;
-        else if (reader.TokenType != JsonToken.String)
-            throw new JsonSerializationException($"Unexpected token parsing date. Expected String, got {reader.TokenType}.");
-
-        string date_text = reader.Value.ToString();
-        if (string.IsNullOrEmpty(date_text) && nullable)
-            return null;
-
-        return DateTime.Parse(date_text, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal | DateTimeStyles.AssumeUniversal);
-    }
-
-    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-    {
-        if (value is DateTime dateTime)
-        {
-            var str = dateTime.ToString(DefaultDateTimeFormat, CultureInfo.InvariantCulture);
-            writer.WriteValue(str);
-        }
-        else
-            throw new JsonSerializationException(
-                $"Unexpected value when converting date. Expected DateTime or DateTimeOffset, got {value.GetType()}.");
-    }
-}
-*/
-
-//https://stackoverflow.com/questions/34185295/handling-null-objects-in-custom-jsonconverters-readjson-method
-public class MillisecondEpochConverter : JsonConverter
-{
-    public override bool CanConvert(Type objectType) => objectType == typeof(DateTime);
-
-    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
-    {
-        if (reader.TokenType == JsonToken.Null) return null;
-        //if (reader.Value is null) return null;
-        var milliseconds = (long?)reader.Value;
-        return milliseconds.HasValue ? milliseconds.Value.FromUnixTimeMS() : (object?)null;
-    }
-
-    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
-    {
-        var dt = value as DateTime?;
-        if (dt.HasValue)
-            writer.WriteValue(dt.Value.ToUnixTimeMS());
-    }
-}
-
 public class ColumnCleaner : JsonConverter
 {
     public override bool CanConvert(Type objectType) => objectType == typeof(DateTime);
@@ -86,20 +19,3 @@ public class ColumnCleaner : JsonConverter
         => throw new NotImplementedException($"{nameof(ColumnCleaner)}.{nameof(WriteJson)} not implemented");
 }
 //todo: Uri convertor
-
-//public class MicrosecondEpochConverter : DateTimeConverterBase
-//{
-//    static readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-//    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-//    {
-//        if (reader.Value is null) { return null; }
-//        var t = long.Parse((string)reader.Value);
-//        return _epoch.AddMilliseconds(t / 1000d);
-//    }
-
-//    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-//    {
-//        writer.WriteRawValue(((DateTime)value - _epoch).TotalMilliseconds + "000");
-//    }
-//}
