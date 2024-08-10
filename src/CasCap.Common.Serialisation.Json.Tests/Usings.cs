1c3362b02eda80627a93039a4750e096d5365001
Alex Vincent <alex@far2cool.com>
2024-08-10 06:29:10 +0200

    cleanup

--- src/CasCap.Common.Serialisation.Json.Tests/Usings.cs
+++
@@ -1,8 +0,0 @@
-﻿global using CasCap.Common.Extensions;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Logging;
-global using Newtonsoft.Json;
-global using System;
-global using System.Collections.Generic;
-global using Xunit;
-global using Xunit.Abstractions;
--- src/CasCap.Common.Serialisation.MessagePack.Tests/Usings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Logging;
-global using System;
-global using Xunit;
-global using Xunit.Abstractions;
