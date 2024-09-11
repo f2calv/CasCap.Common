168396c79a2d85f4591829e0979396d0807ca95f
Alex Vincent <alex@far2cool.com>
2024-09-11 09:41:45 +0200

    tweak logic

--- src/CasCap.Common.Caching/Services/DistributedCacheService.cs
+++ src/CasCap.Common.Caching/Services/DistributedCacheService.cs
@@ -27,7 +27,7 @@ public class DistributedCacheService(ILogger<DistributedCacheService> logger,
             logger.LogTrace("{serviceName} unable to retrieve {key} object type {type} from local cache",
                 nameof(DistributedCacheService), key, typeof(T));
             var tpl = await remoteCacheSvc.GetCacheEntryWithTTL<T>(key);
-            if (tpl != default)
+            if (tpl != default && tpl.cacheEntry is not null)
             {
                 logger.LogTrace("{serviceName} retrieved {key} object type {type} from remote cache",
                     nameof(DistributedCacheService), key, typeof(T));
