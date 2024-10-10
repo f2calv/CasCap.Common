217b8512fc5f62d06b865db4835b8aa1e5908ed0
Alex Vincent <alex@far2cool.com>
2024-10-10 03:59:52 +0200

    system.text.json converters updated

--- src/CasCap.Common.Serialization.Json/Converters/MillisecondEpochConverter.cs
+++ src/CasCap.Common.Serialization.Json/Converters/MillisecondEpochConverter.cs
@@ -1,26 +1,16 @@
-﻿/*
-namespace CasCap.Models;
+﻿namespace CasCap.Models;
 
-/// <summary>
-/// https://stackoverflow.com/questions/34185295/handling-null-objects-in-custom-jsonconverters-readjson-method
-/// </summary>
-public class MillisecondEpochConverter : JsonConverter
+public class MillisecondEpochConverter : JsonConverter<DateTime?>
 {
-    public override bool CanConvert(Type objectType) => objectType == typeof(DateTime);
-
-    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        if (reader.TokenType == JsonToken.Null) return null;
-        //if (reader.Value is null) return null;
-        var milliseconds = (long?)reader.Value;
-        return milliseconds.HasValue ? milliseconds.Value.FromUnixTimeMS() : (object?)null;
+        if (reader.TokenType == JsonTokenType.Null) return null;
+        return long.TryParse(reader.GetString(), out var t) ? t.FromUnixTimeMS() : null;
     }
 
-    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    public override void Write(Utf8JsonWriter writer, DateTime? dateTimeValue, JsonSerializerOptions options)
     {
-        var dt = value as DateTime?;
-        if (dt.HasValue)
-            writer.WriteValue(dt.Value.ToUnixTimeMS());
+        if (dateTimeValue.HasValue)
+            writer.WriteRawValue(dateTimeValue.Value.ToUnixTimeMS().ToString());
     }
 }
-*/
--- src/CasCap.Common.Serialization.Json/Usings.cs
+++ src/CasCap.Common.Serialization.Json/Usings.cs
@@ -1,3 +1,5 @@
-﻿global using Microsoft.Extensions.Logging;
+﻿global using CasCap.Common.Extensions;
+global using Microsoft.Extensions.Logging;
 global using System;
 global using System.Text.Json;
+global using System.Text.Json.Serialization;
