47e3b33d446d45be1576ede95e3d6f807c5fa227
Alex Vincent <alex@far2cool.com>
2024-12-13 21:41:38 +0100

    added FixedSizeQueue

---
+++ src/CasCap.Common.Extensions/Models/FixedSizeQueue.cs
@@ -0,0 +1,55 @@
+﻿namespace CasCap.Models;
+
+//https://stackoverflow.com/questions/5852863/fixed-size-queue-which-automatically-dequeues-old-values-upon-new-enques
+[Serializable]
+[DebuggerDisplay("Count = {" + nameof(Count) + "}, Limit = {" + nameof(Limit) + "}")]
+public class FixedSizedQueue<T> : IReadOnlyCollection<T>
+{
+    private readonly Queue<T> _queue = new();
+    private readonly object _lock = new();
+
+    public int Count { get { lock (_lock) { return _queue.Count; } } }
+    public int Limit { get; }
+
+    public FixedSizedQueue(int limit)
+    {
+        if (limit < 1)
+            throw new ArgumentOutOfRangeException(nameof(limit));
+
+        Limit = limit;
+    }
+
+    public FixedSizedQueue(IEnumerable<T> collection)
+    {
+        if (collection is null || !collection.Any())
+            throw new ArgumentException("Can not initialize the Queue with a null or empty collection", nameof(collection));
+
+        _queue = new Queue<T>(collection);
+        Limit = _queue.Count;
+    }
+
+    public void Enqueue(T obj)
+    {
+        lock (_lock)
+        {
+            _queue.Enqueue(obj);
+
+            while (_queue.Count > Limit)
+                _queue.Dequeue();
+        }
+    }
+
+    public void Clear()
+    {
+        lock (_lock)
+            _queue.Clear();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        lock (_lock)
+            return new List<T>(_queue).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
--- src/CasCap.Common.Extensions/Usings.cs
+++ src/CasCap.Common.Extensions/Usings.cs
@@ -1,5 +1,7 @@
 ﻿global using System;
+global using System.Collections;
 global using System.Collections.Generic;
+global using System.Diagnostics;
 global using System.IO;
 global using System.Linq;
 global using System.Runtime.CompilerServices;
