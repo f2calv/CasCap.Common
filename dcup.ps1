2fa559fee9211b5bea345d67481b2b2a5f185d3f
Alex Vincent <alex@far2cool.com>
2025-01-15 14:54:21 +0100

    update

--- dcup.ps1
+++ dcup.ps1
@@ -1,5 +1,6 @@
 #.NET Application environment variables
 $env:DOTNET_ENVIRONMENT = "Development"
+$env:ASPDOTNET_ENVIRONMENT = "Development"
 $env:DOTNET_RUNNING_IN_COMPOSE = $true
 
 docker-compose up --build --remove-orphans
