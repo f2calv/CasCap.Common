351b293d89cb7d246f5bd89e2e674c4a11eb434b
Alex Vincent <alex@far2cool.com>
2025-02-24 15:51:01 +0100

    consolidate

---
+++ src/CasCap.Common.Serialization.Json/Converters/StringToIntConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace CasCap.Converters;
+
+//TODO: move to common lib although this can be handled better by generics or even better by JsonNumberHandling.AllowReadingFromString
+public class StringToIntConverter : JsonConverter<int?>
+{
+    public override int? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Null) return null;
+        return int.TryParse(reader.GetString(), out var t) ? t : null;
+    }
+
+    public override void Write(Utf8JsonWriter writer, int? intValue, JsonSerializerOptions options)
+    {
+        if (intValue.HasValue)
+            writer.WriteRawValue(intValue.ToString());
+    }
+}
