b12d84f62219f646d7183aad6ee2726b806eeaa9
Alex Vincent <alex@far2cool.com>
2025-03-10 03:36:42 +0100

    xml comments

--- src/CasCap.Common.Logging/Logging/ApplicationLogging.cs
+++ src/CasCap.Common.Logging/Logging/ApplicationLogging.cs
@@ -4,8 +4,13 @@
 public static class ApplicationLogging
 {
 #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+    /// <inheritdoc cref="ILoggerFactory"/>
     public static ILoggerFactory LoggerFactory { get; set; }
 #pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+
+    /// <inheritdoc cref="LoggerFactoryExtensions.CreateLogger{T}(ILoggerFactory)"/>
     public static ILogger CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
+
+    /// <inheritdoc cref="ILoggerFactory.CreateLogger(string)"/>
     public static ILogger CreateLogger(string categoryName) => LoggerFactory.CreateLogger(categoryName);
 }
