b8f2e7074b7829b529970a6a6b870906736fc54d
Alex Vincent <alex@far2cool.com>
2025-09-30 03:50:11 +0200

    disable

--- src/CasCap.Common.Logging/Extensions/LoggingExtensions.cs
+++ src/CasCap.Common.Logging/Extensions/LoggingExtensions.cs
@@ -1,11 +1,11 @@
-﻿namespace Microsoft.Extensions.Logging;
+﻿//namespace Microsoft.Extensions.Logging;
 
-public static class LoggingExtensions
-{
-#pragma warning disable IDE0060 // Remove unused parameter
-    //public static void LogError(this ILogger logger, Exception ex, [CallerMemberName] string caller = "")
-    //{
-    //    logger.LogError(ex, "Caller={caller}, Exception={@exception}", caller, ex);//3rd parameter to prevent infinite loop
-    //}
-#pragma warning restore IDE0060 // Remove unused parameter
-}
+//public static class LoggingExtensions
+//{
+//#pragma warning disable IDE0060 // Remove unused parameter
+//    //public static void LogError(this ILogger logger, Exception ex, [CallerMemberName] string caller = "")
+//    //{
+//    //    logger.LogError(ex, "Caller={caller}, Exception={@exception}", caller, ex);//3rd parameter to prevent infinite loop
+//    //}
+//#pragma warning restore IDE0060 // Remove unused parameter
+//}
