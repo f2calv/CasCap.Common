f972594a58a5aaaf06bdeb77410d93173e580209
Alex Vincent <alex@far2cool.com>
2026-04-13 01:46:31 +0200

    mv ClientWebSocketExtensions to CasCap.Common.Net

---
+++ src/CasCap.Common.Net/Extensions/ClientWebSocketExtensions.cs
@@ -0,0 +1,21 @@
+using System.Net.WebSockets;
+
+namespace CasCap.Common.Extensions;
+
+/// <summary>Convenience helpers for <see cref="ClientWebSocket"/>.</summary>
+public static class ClientWebSocketExtensions
+{
+    /// <summary>
+    /// Sets the <c>Authorization</c> header to HTTP Basic on a <see cref="ClientWebSocket"/>.
+    /// </summary>
+    /// <remarks>WebSocket counterpart of <see cref="NetExtensions.SetBasicAuth(HttpClient, string, string)"/>.</remarks>
+    /// <param name="ws">The <see cref="ClientWebSocket"/> to configure.</param>
+    /// <param name="username">The Basic authentication username.</param>
+    /// <param name="password">The Basic authentication password.</param>
+    public static void SetBasicAuth(this ClientWebSocket ws, string username, string password)
+    {
+        var base64 = Convert.ToBase64String(
+            Encoding.ASCII.GetBytes($"{username}:{password}"));
+        ws.Options.SetRequestHeader("Authorization", $"Basic {base64}");
+    }
+}
