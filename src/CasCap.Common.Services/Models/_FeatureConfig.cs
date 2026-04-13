635614e60bf7bfc64e30ad8dcf8b2b1dbce71efe
Alex Vincent <alex@far2cool.com>
2026-04-13 02:21:20 +0200

    re-add IAppConfig to FeatureConfig

--- src/CasCap.Common.Services/Models/_FeatureConfig.cs
+++ src/CasCap.Common.Services/Models/_FeatureConfig.cs
@@ -1,9 +1,12 @@
 ﻿namespace CasCap.Common.Models;
 
 /// <inheritdoc cref="IFeatureConfig{T}"/>
-public record FeatureConfig<T> : IFeatureConfig<T>
+public record FeatureConfig<T> : IAppConfig, IFeatureConfig<T>
     where T : Enum
 {
+    /// <inheritdoc/>
+    public static string ConfigurationSectionName => $"{nameof(CasCap)}:{nameof(FeatureConfig<T>)}";
+
     /// <inheritdoc/>
     [Required]
     public required T AppMode { get; init; }
