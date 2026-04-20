036c3a2f9e9b4ab94b020c23271341da39dcf296
Alex Vincent <alex@far2cool.com>
2026-04-20 17:12:47 +0200

    fix validation bug

--- src/CasCap.Common.Configuration/GlobalUsings.cs
+++ src/CasCap.Common.Configuration/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
+global using System.ComponentModel.DataAnnotations;
